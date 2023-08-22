32233e5063f75601e8ee588dcbf6027777a2527d
nikrassel <nikrassel@mail.ru>
2023-08-22 21:23:31 +0300

    add base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Main from "./pages/main";
+import BrowseInfo from "./pages/browseInfo";
+import Login from "./pages/login";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
-    <div className="App">
-      <h1>New App</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/browse" element={<BrowseInfo />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/browseInfo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BrowseInfo = () => {
+  return <h1>Browse Info</h1>;
+};
+
+export default BrowseInfo;
