4a4836b5981531d3f43781b9aed816ba048b1022
nikrassel <nikrassel@mail.ru>
2023-08-23 21:49:03 +0300

    add sorting for child component

--- src/components/Info/childComponent.tsx
+++ src/components/Info/childComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import _ from "lodash";
 import TextField from "../form/textField";
 
 type InfoObject = {
@@ -13,6 +14,21 @@ type ChildComponentProps = {
 
 const ChildComponent: React.FC<ChildComponentProps> = ({ info }) => {
   const [search, setSearch] = React.useState("");
+  const [sortBy, setSortBy] = React.useState<{
+    onSort: boolean;
+    order: "asc" | "desc";
+  }>({ onSort: false, order: "asc" });
+  React.useEffect(() => {
+    setSearch("");
+    setSortBy({ onSort: false, order: "asc" });
+  }, [info]);
+  function handleSort() {
+    if (!sortBy.onSort || sortBy.order !== "asc") {
+      setSortBy({ onSort: true, order: "asc" });
+    } else {
+      setSortBy({ onSort: true, order: "desc" });
+    }
+  }
   if (info.children.length !== 0) {
     let filtredInfo = [...info.children];
     if (search) {
@@ -20,6 +36,9 @@ const ChildComponent: React.FC<ChildComponentProps> = ({ info }) => {
       filtredInfo = filtredInfo.filter((info) =>
         nameRegExp.test(info.name.toLowerCase())
       );
+    }
+    if (sortBy.onSort) {
+      filtredInfo = _.orderBy(filtredInfo, "name", sortBy.order);
     }
     console.log(filtredInfo);
     return (
@@ -31,6 +50,18 @@ const ChildComponent: React.FC<ChildComponentProps> = ({ info }) => {
           onChange={setSearch}
           placeHolder="Поиск..."
         />
+        <button
+          type="button"
+          className="btn btn-primary mb-4"
+          onClick={() => handleSort()}
+        >
+          {sortBy.order === "asc" ? (
+            <i className="bi bi-caret-up-fill"></i>
+          ) : (
+            <i className="bi bi-caret-down-fill"></i>
+          )}
+          Сортировка
+        </button>
         <ul>
           {filtredInfo.map((elem) => (
             <li key={elem.key}>{elem.name}</li>
--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,13 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <>
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+    ></link>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </>
 );
